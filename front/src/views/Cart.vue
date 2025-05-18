0d97fdb51270a814128dc0f5f444f0732427eb74
JoharyJonathan <rjoharyjonathan@gmail.com>
2025-05-18 18:01:55 +0300

    Articles count in cart view

--- front/src/views/Cart.vue
+++ front/src/views/Cart.vue
@@ -9,7 +9,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
-              <span class="font-bold">3 articles</span>
+              <span class="font-bold">{{ cart.items.length }} articles</span>
             </div>
           </div>
         </div>
@@ -130,7 +130,7 @@
           const response = await axios.get(`http://localhost:8080/api/carts/${userId}`);
 
           this.cart = response.data;
-          console.log(this.cart);
+          console.log(this.cart.items[0]);
 
           for (const item of this.cart.items) {
             await this.getProduct(item.productId);
@@ -155,10 +155,12 @@
       },
       increaseQuantity(product) {
         product.quantity++;
+        this.updateProductQuantity(product.productId, product.quantity);
       },
       decreaseQuantity(product) {
         if (product.quantity > 1) {
           product.quantity--;
+          this.updateProductQuantity(product.productId, product.quantity);
         }
       },
       async updateProductQuantity(productId, quantity) {
@@ -170,7 +172,6 @@
             "quantity": quantity
           });
           console.log(response.data);
-          alert('Product number as been updated');
         } catch (error) {
           console.error('Error updating product : ', error);
         }
