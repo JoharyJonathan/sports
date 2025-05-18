9c1f673155faddbe371bbd715fbfbd4722cbac43
JoharyJonathan <rjoharyjonathan@gmail.com>
2025-05-18 18:28:02 +0300

    Remove product in cart

--- front/src/views/Cart.vue
+++ front/src/views/Cart.vue
@@ -10,6 +10,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
               <span class="font-bold">{{ cart.items.length }} articles</span>
+              <button class="bg-red-500 hover:bg-red-400 text-white py-1 px-4 rounded-2xl ml-6">
+                supprimer mon panier
+              </button>
             </div>
           </div>
         </div>
@@ -40,7 +43,7 @@
                     <input class="w-10 h-10" :value=product.quantity disabled>
                     <button @click="increaseQuantity(product)" class="px-3 py-1 text-blue-300 hover:text-white">+</button>
                   </div>
-                  <button class="text-red-400 hover:text-red-300 flex items-center">
+                  <button @click="removeProduct(product.productId)" class="text-red-400 hover:text-red-300 flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
@@ -174,6 +177,19 @@
           console.log(response.data);
         } catch (error) {
           console.error('Error updating product : ', error);
+        }
+      },
+      async removeProduct(productId) {
+        const userId = this.$route.params.id;
+
+        try {
+          const response = await axios.delete(`http://localhost:8080/api/carts/${userId}/${productId}`);
+
+          console.log(response);
+          alert('Produit supprime du panier');
+          window.location.reload();
+        } catch (error) {
+          console.error('Error removing product : ', error);
         }
       }
     }
